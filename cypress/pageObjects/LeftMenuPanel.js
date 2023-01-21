e5c3e835cf78fb561fd24d9ca21b36a14bb3f072
Alex Mok <58538625+AlexMokeev@users.noreply.github.com>
2023-01-21 00:15:12 -0500

    AT_03.03.01 | Left Menu Panel > Verify the "Bookings management" icon is visible (#24)
    * TC_03.03.01 Bookings management icon is visible

--- cypress/pageObjects/LeftMenuPanel.js
+++ cypress/pageObjects/LeftMenuPanel.js
@@ -1,6 +1,7 @@
 class LeftMenuPanel {
     elements = {
         getMainElements: () => cy.get('.main-sidebar i'),
+        getBookingManagement: () => cy.get('a[href="/orders/"] i'),
         getMainElementsNames: () => cy.get('.main-sidebar span')
     }
 }
