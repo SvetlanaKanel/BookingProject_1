3143db9c41367d0185862b973e7658a61636f1d7
Rom-T <93357374+Rom-T@users.noreply.github.com>
2023-01-22 13:57:32 -0500

    AT_01.05.03 | Login by email tab UI > Verify the agent is able to see Email input field and it has text 'Email' (#22)
    * AT_01.05.03

--- cypress/e2e/testUiAndFunction/startPage/loginPopupSection/US_01.05_LoginByEmailTab_UI.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/loginPopupSection/US_01.05_LoginByEmailTab_UI.cy.js
@@ -7,12 +7,22 @@ const startPage = new StartPage();
 const loginPopup = new LoginPopup();
 
 describe('US_01.05 | Login By Email Tab UI', () => {
-    beforeEach(() => {
-        cy.visit('/')
-        startPage.clickLoginButton()
+    beforeEach(function () {
+        cy.fixture('startPage/inputField').then(inputField => {
+            this.inputField = inputField;
+        });
+        cy.visit('/');
+        startPage.clickLoginButton();
     });
 
     it('AT_01.05.01 | Insure By Email tab is visible', () => {
         loginPopup.getLoginByEmailTab().should('be.visible')
-    })
+    });
+
+    it('AT_01.05.03 | Verify the agent is able to see Email input field and it has text "Email"', function () {
+        loginPopup
+            .getEmailInput()
+            .should('be.visible')
+            .and('have.attr', 'placeholder', this.inputField.loginPopup.emailInputField);
+    });
 })
--- cypress/pageObjects/StartPage.js
+++ cypress/pageObjects/StartPage.js
@@ -20,7 +20,8 @@ export class LoginPopup {
     
     getLoginByEmailTab = () => cy.get('[href="#byemail"]');
     getForgotYourPasswordLink = () => cy.get('#loginModal .pull-right a');
-    getHeaderTextElement = () => cy.get('.text-center')
+    getEmailInput = () => cy.get('#byemail input[placeholder="Email"]');
+    getHeaderTextElement = () => cy.get('.text-center');
 
 
     // Methods
