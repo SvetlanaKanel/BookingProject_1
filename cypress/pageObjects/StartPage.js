4f44348a353ac55f40a1da1daadaeccba1237140
Rom-T <93357374+Rom-T@users.noreply.github.com>
2023-01-21 21:58:39 -0500

    AT_01.19.01 | Start page > Verify message after input an existing email in the "Email" input field and clicking on the "RESTORE" button (#21)
    * AT_01.19.01

--- cypress/pageObjects/StartPage.js
+++ cypress/pageObjects/StartPage.js
@@ -10,11 +10,35 @@ export class StartPage {
     }
 }
 
- export class LoginPopup {
+export class LoginPopup {
     //Elements
     
-    getLoginByEmailTab = () => cy.get('[href="#byemail"]')
+    getLoginByEmailTab = () => cy.get('[href="#byemail"]');
+    getForgotYourPasswordLink = () => cy.get('#loginModal .pull-right a');
 
 
     // Methods
- }
+
+    clickForgotYourPasswordLink() {
+        this.getForgotYourPasswordLink().click();
+    };
+}
+
+export class RestorePopup {
+    //Elements
+    
+    getEmailInput = () => cy.get('#restoreModal input[placeholder="Email"]');
+    getRestoreButton = () => cy.get('#restoreModal input[type="submit"]');
+    getMessageAlert = () => cy.get('#restoreModal div.alert');
+
+
+    // Methods
+
+    enterEmail(agentEmail) {
+        this.getEmailInput().type(agentEmail, { force: true });
+    };
+
+    clickRestoreButton() {
+        this.getRestoreButton().click();
+    };
+}
