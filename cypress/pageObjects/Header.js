64623a6aa7896525c782cf8e11e24807a126a61b
OLgaBuR <olbur12@gmail.com>
2023-01-21 23:35:52 -0600

    first commit

--- cypress/pageObjects/Header.js
+++ cypress/pageObjects/Header.js
@@ -1,6 +1,10 @@
 class Header {
-    elements = {
+    getUserDropDownMenu = () => cy.get('.dropdown.user.user-menu')
+    getLanguageIcons = () => cy.get('.user-header .user-lang-selection li')
 
+    clickUserDropDownMenu() {
+        this.getUserDropDownMenu().click()
     }
+
 }
 export default Header;
