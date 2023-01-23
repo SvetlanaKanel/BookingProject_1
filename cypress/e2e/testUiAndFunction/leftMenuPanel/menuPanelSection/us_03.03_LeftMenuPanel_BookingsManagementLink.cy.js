971b8badc869388c47c805070958adbf282c0c40
Alex Mok <58538625+AlexMokeev@users.noreply.github.com>
2023-01-22 20:50:03 -0500

    AT_03.03.02 Verify text "Booking management" (#35)
    * AT_03.03.02 Verify text "Booking management"

--- cypress/e2e/testUiAndFunction/leftMenuPanel/menuPanelSection/us_03.03_LeftMenuPanel_BookingsManagementLink.cy.js
+++ cypress/e2e/testUiAndFunction/leftMenuPanel/menuPanelSection/us_03.03_LeftMenuPanel_BookingsManagementLink.cy.js
@@ -4,17 +4,25 @@ import LeftMenuPanel from "../../../../pageObjects/LeftMenuPanel";
 
 const leftMenuPanel = new LeftMenuPanel();
 
-describe('US_03.03 Left Menu Panel > Bookings management link', () => {
+describe('US_03.03 Bookings management link', () => {
     
     const AGENT = Cypress.env('agent');
 
     beforeEach(function () {
+        cy.fixture('leftMenuPanel/menuLinks').then(link => {
+            this.link = link;
+        });
         cy.visit('/');
+        cy.login(AGENT.email, AGENT.password);
     });
 
-    it('AT_03.03.01 Verify the "Bookings management" icon is visible', () => {
-        cy.login(AGENT.email, AGENT.password);
-
+    it('AT_03.03.01 Verify the "Bookings management" icon is visible', () => {  
         leftMenuPanel.getBookingManagement().should('be.visible')
+    });
+
+    it('AT_03.03.02 Verify the Sidebar has text "Booking management" ', function () {
+        leftMenuPanel
+            .getBookingManagementNameLink()
+            .should('include.text', this.link.bookingsManagementLink)
     });
 });
