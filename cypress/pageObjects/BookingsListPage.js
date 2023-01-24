6f3523d246f76fa0dfded2b7a9f07db418c54acf
EkaterinaKaverina <117792031+EkaterinaKaverina@users.noreply.github.com>
2023-01-24 17:13:15 -0500

    AT_05.06.02 (#76)

--- cypress/pageObjects/BookingsListPage.js
+++ cypress/pageObjects/BookingsListPage.js
@@ -1,6 +1,7 @@
 class BookingsListPage {
     getBookingListHeader = () => cy.get('.page-title');
     getPrintButton = () => cy.get('.row .btn-print');
+    getPrintButtonIcon = () => cy.get('.row .btn-print i');
 
     clickPrintButton() {
         this.clickPrintButton().click();
