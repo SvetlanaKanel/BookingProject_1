47a91ae25b411204e4841e6b3fdf46c603975fcf
OLgaBuR <olbur12@gmail.com>
2023-01-24 08:45:17 -0600

    first commit

--- cypress/pageObjects/CreateBookingPage.js
+++ cypress/pageObjects/CreateBookingPage.js
@@ -3,9 +3,10 @@ class CreateBookingPage {
     getMainPassengerNameField = () => cy.get('.form-control[name="passenger-name[]"]');
     getCalendarNextButton = () => cy.get('div .calendar-week-next');
     getFirstTripCard = () => cy.get('div .trip:first-child');
-    getCreateBookingHeader = () => cy.get('div h1')
+    getCreateBookingHeader = () => cy.get('div h1');
     getCalendarDaySelectionWrapper = () => cy.get('.col-lg-12.calendar-day-selection-wrapper .day-wrapper');
     getLabelDepartureOnDate = () => cy.get('#label-departure-on #trips-selected-date');
+    getLabelCalendarWeek = () => cy.get('.calendar-week-selection-wrapper #calendar-week');
     
     // Methods
     clickCalendarNextButton() {
