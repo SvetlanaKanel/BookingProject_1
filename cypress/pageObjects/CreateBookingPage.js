53f0840c693c9b71810964abdfdf4e2185b36394
Olga <oforostinko@gmail.com>
2023-01-25 18:25:25 +0300

    fixed

--- cypress/pageObjects/CreateBookingPage.js
+++ cypress/pageObjects/CreateBookingPage.js
@@ -5,9 +5,8 @@ class CreateBookingPage {
     getCalendarNextButton = () => cy.get('div .calendar-week-next');
     getFirstTripCard = () => cy.get('div .trip:first-child');
     getCreateBookingHeader = () => cy.get('div h1');
-    getCalendarDaySelectionWrapper = () => cy.get('.col-lg-12.calendar-day-selection-wrapper .day-wrapper');
+    getCalendarDays = () => cy.get('.col-lg-12.calendar-day-selection-wrapper .day-wrapper');
     getLabelDepartureOnDate = () => cy.get('#label-departure-on #trips-selected-date');
-    getLabelCalendarWeek = () => cy.get('.calendar-week-selection-wrapper #calendar-week');
     getLabelCalendar = () => cy.get('div #calendar-week');
     getLabelPassengerDetails = () => cy.get('.passenger-wrapper div.title label');
     getMonthBtn = () => cy.get('.calendar-view-wrapper .calendar-view-month');
