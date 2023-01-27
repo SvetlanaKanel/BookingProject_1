367faae06f8c63f4f420fd68203d8d26b31e94bf
LauraB <wavenlondon@gmail.com>
2023-01-27 17:50:16 +0000

    AT_01.18.01 | Verify the link "Register" is clickable and redirects to the "Register agent account" popup'

--- cypress/pageObjects/StartPage.js
+++ cypress/pageObjects/StartPage.js
@@ -71,7 +71,8 @@ export class RestorePopup {
     getMessageAlert = () => cy.get('#restoreModal div.alert');
     getRestorePasswordHeader = () => cy.get('#restoreModal h2');
     getCloseButton = () => cy.get('div#restoreModal .modal-header .close');
-    getRestorePopup = () => cy.get('div#restoreModal')
+    getRestorePopup = () => cy.get('div#restoreModal');
+    getRestorePopupRegisterLink = () => cy.get('.form-horizontal .modal-footer .pull-left a');
 
     // Methods
 
@@ -85,6 +86,10 @@ export class RestorePopup {
 
     clickCloseButton(){
         this.getCloseButton().click()
+    };
+
+    clickRestorePopupRegisterLink(){
+        this.getRestorePopupRegisterLink().click();
     }
 }
 
