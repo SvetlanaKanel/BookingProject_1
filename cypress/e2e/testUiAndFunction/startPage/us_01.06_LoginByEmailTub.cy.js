653f6d3c4a4cfab5a612fe342597afb87de47aad
Olga <oforostinko@gmail.com>
2023-01-27 18:13:45 +0300

    fixed code

--- cypress/e2e/testUiAndFunction/startPage/us_01.06_LoginByEmailTub.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/us_01.06_LoginByEmailTub.cy.js
@@ -1,10 +1,12 @@
 /// <reference types="Cypress" />
 
-import CreateBookingPage from "../../../../pageObjects/CreateBookingPage";
-import { LoginPopup } from "../../../../pageObjects/StartPage";
+import CreateBookingPage from "../../../pageObjects/CreateBookingPage";
+import { LoginPopup } from "../../../pageObjects/StartPage";
+import { StartPage } from "../../../pageObjects/StartPage";
 
 const createBookingPage = new CreateBookingPage(); 
 const loginPopup = new LoginPopup();
+const startPage = new StartPage();
 
 describe('US_01.06 | Login by email tub functionality', () => {
 
@@ -14,10 +16,10 @@ describe('US_01.06 | Login by email tub functionality', () => {
         cy.fixture('createBookingPage').then(createBookingPage => {
             this.createBookingPage = createBookingPage
         });
-        cy.fixture('startPage/buttons').then(button => {
-            this.button = button
+        cy.fixture('startPage').then(startPage => {
+            this.startPage = startPage
         });
-        cy.visit('/');    
+        cy.visit('/');  
     });
 
     it('AT_01.06.01 | Verify Sign In Button redirect to the Create Booking Page', function () {
@@ -26,8 +28,8 @@ describe('US_01.06 | Login by email tub functionality', () => {
     });
 
     it('AT_01.06.02 | Color of Sign In Button', function () {
-        cy.login(AGENT.email, AGENT.password);
-        loginPopup.getSignInButton().should('have.css','color', this.button.signInButton.color)
+        startPage.clickLoginButton();
+        loginPopup.getSignInButton().should('have.css','color', this.startPage.buttons.signInBtnTextColor)
     });
 });
 
