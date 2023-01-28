41bead239a622fca7a60ccb829e23bdef92ec811
SvetlanaG1011 <103342736+SvetlanaG1011@users.noreply.github.com>
2023-01-27 18:08:50 -0600

    AT_01.15.08 | Error message is displayed when trying to register with invalid company name (1 letter and 1 number without space , example: "A1")' (#127)
    * "AT_01.15.08"
    
    * edited formatting

--- cypress/e2e/testUiAndFunction/startPage/us_01.15_RegisterAgentNegative.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/us_01.15_RegisterAgentNegative.cy.js
@@ -6,13 +6,17 @@ import { faker } from '@faker-js/faker';
 
 const startPage = new StartPage();
 const registerPopup = new RegisterPopup();
-const randomFirstName = faker.name.firstName(); 
-const randomLastName = faker.name.lastName(); 
+const randomFirstName = faker.name.firstName();
+const randomLastName = faker.name.lastName();
 const randomFullName = `${randomFirstName} + ${randomLastName}`;
 const randomEmail = faker.internet.email(randomFirstName, randomLastName, 'qatest.site');
 const randomInvalidEmail = faker.internet.email('a-g@ent', '-@-12', '--@qatest.site');
 const randomPhoneNumber = faker.phone.number('+66#########');
 const randomCompanyName = faker.company.name();
+const randomAlpha = faker.random.alpha();
+const randomNumeric = faker.random.numeric();
+const randomInvalidCompanyName = `${randomAlpha + randomNumeric}`;
+
 
 describe('US_01.15 | Register Agent Negative', function () {
 
@@ -47,7 +51,7 @@ describe('US_01.15 | Register Agent Negative', function () {
             .and('have.text', this.startPage.alert.registerPopupErrorMessage.emptyCompanyField)
     });
 
-    it('AT_01.15.03 | Error message is displayed when trying to register without email', function() {
+    it('AT_01.15.03 | Error message is displayed when trying to register without email', function () {
         registerPopup.enterName(randomFullName)
         registerPopup.enterCompanyName(randomCompanyName)
         registerPopup.enterPhoneNumber(randomPhoneNumber)
@@ -58,7 +62,7 @@ describe('US_01.15 | Register Agent Negative', function () {
             .and('have.text', this.startPage.alert.registerPopupErrorMessage.emptyEmailField)
     });
 
-    it('AT_01.15.05 | Error message is displayed when trying to register with invalid email', function() {
+    it('AT_01.15.05 | Error message is displayed when trying to register with invalid email', function () {
         registerPopup.enterName(randomFullName)
         registerPopup.enterCompanyName(randomCompanyName)
         registerPopup.enterEmail(randomInvalidEmail)
@@ -70,17 +74,17 @@ describe('US_01.15 | Register Agent Negative', function () {
             .and('have.text', this.startPage.alert.registerPopupErrorMessage.emptyEmailField)
     });
 
-    it('AT_01.15.04 | Error message is displayed when trying to register without phone number', function() {
+    it('AT_01.15.04 | Error message is displayed when trying to register without phone number', function () {
         registerPopup.enterName(randomFullName)
         registerPopup.enterCompanyName(randomCompanyName)
         registerPopup.enterEmail(randomEmail)
         registerPopup.clickRegisterButton()
         registerPopup.getErrorMessage()
-                     .should('be.visible')
-                     .and('have.text', this.startPage.alert.registerPopupErrorMessage.emptyPhoneField)
+            .should('be.visible')
+            .and('have.text', this.startPage.alert.registerPopupErrorMessage.emptyPhoneField)
     });
 
-    it('AT_01.15.07 | Error message is displayed when trying to register  with previously used email', function() {
+    it('AT_01.15.07 | Error message is displayed when trying to register  with previously used email', function () {
         registerPopup.enterName(randomFullName)
         registerPopup.enterCompanyName(randomCompanyName)
         registerPopup.enterEmail(this.startPage.data.previouslyUsedEmail)
@@ -92,4 +96,15 @@ describe('US_01.15 | Register Agent Negative', function () {
             .and('have.text', this.startPage.alert.registerPopupErrorMessage.emailIsPreviouslyUsed)
     });
 
+    it('AT_01.15.08 | Error message is displayed when trying to register with invalid company name (1 letter and 1 number without space , example: "A1")', function () {
+        registerPopup.enterName(randomFullName)
+        registerPopup.enterCompanyName(randomInvalidCompanyName)
+        registerPopup.enterEmail(randomEmail)
+        registerPopup.enterPhoneNumber(randomPhoneNumber)
+        registerPopup.clickRegisterButton()
+        registerPopup
+            .getErrorMessage()
+            .should('be.visible')
+            .and('have.text', this.startPage.alert.registerPopupErrorMessage.emptyCompanyField)
+    });
 })
