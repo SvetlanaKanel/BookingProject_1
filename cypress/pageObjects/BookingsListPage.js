57e9c3aa96bc42809fc52368c79c8b9c5b0adadd
Alzhan-B <115260318+Alzhan-B@users.noreply.github.com>
2023-01-31 00:17:48 -0500

    AT_05.10.02 (#164)

--- cypress/pageObjects/BookingsListPage.js
+++ cypress/pageObjects/BookingsListPage.js
@@ -3,6 +3,7 @@ class BookingsListPage {
     getPrintButton = () => cy.get('.row .btn-print');
     getPrintButtonIcon = () => cy.get('.row .btn-print i');
     getExcelButton = () => cy.get('.row .btn-excel');
+    getExcelButtonIcon = () => cy.get('.row .btn-excel i');
 
     clickPrintButton() {
         this.clickPrintButton().click();
