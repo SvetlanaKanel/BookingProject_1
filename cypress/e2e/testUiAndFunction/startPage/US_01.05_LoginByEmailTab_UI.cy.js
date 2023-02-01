742919ff2ebfa2ade42d835a113de06d3a326169
TimothyHuxx <90024242+TimothyHuxx@users.noreply.github.com>
2023-01-31 18:59:03 -0800

    Add AT_01.05.10 (#178)

--- cypress/e2e/testUiAndFunction/startPage/US_01.05_LoginByEmailTab_UI.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/US_01.05_LoginByEmailTab_UI.cy.js
@@ -58,4 +58,12 @@ describe('US_01.05 | Login By Email Tab UI', () => {
             .getSignInButton()
             .should('have.value', this.startPage.buttons.signInBtnText)
     });
+
+    it('AT_01.05.10 | Verify SIGN IN button is visible and clickable', function() {
+        loginPopup
+            .clickSignInButton()
+
+        loginPopup.getMessageAlert()
+            .should('have.text', this.startPage.alert.loginPopupMessageAlert)
+    });
 });
