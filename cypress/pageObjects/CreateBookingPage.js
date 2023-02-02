b2442c92bf670376edacd79719d0ff892c297565
SvetlanaKanel <svetlana.kanel@gmail.come>
2023-02-01 20:30:27 -0500

    RF CreateBookingPage > add description of sections

--- cypress/pageObjects/CreateBookingPage.js
+++ cypress/pageObjects/CreateBookingPage.js
@@ -1,44 +1,55 @@
-class CreateBookingPage {
-    //Elements
-    getMainPassengerNameField = () => cy.get('.form-control[name="passenger-name[]"]');
-    getMainPassengerPhoneField = () => cy.get('.iti #passenger-phone');
+class CreateBookingPage {   
+    //Header
+    getCreateBookingHeader = () => cy.get('div h1');
+
+    //Departure/Arrival station
+    getDepartureStationDropdown = () => cy.get('#select2-departure-container');
+    getListDepartureStation = () => cy.get('.select2-results .select2-results__option');
+    getArrivalStationDropdown = () => cy.get('#select2-destination-container');
+    getArrivalStationList = () => cy.get('.select2-results .select2-results__options');
+    getDepartureStationSelectionDropdown = () => cy.get('select[name="departure"]');
+    getArrivalStationSelectionDropdown = () => cy.get('select[name="destination"]');
+
+    //Departure date
     getCalendarNextButton = () => cy.get('div .calendar-week-next');
     getCalendarPrevButton = () => cy.get('button.calendar-week-prev');
-    getFirstTripCard = () => cy.get('div .trip:first-child');
-    getTicketsAvailableFirstTripCard = () => cy.get('.trip:first-child span.availability span.num');
-    getCreateBookingHeader = () => cy.get('div h1');
     getCalendarDays = () => cy.get('.col-lg-12.calendar-day-selection-wrapper > .day-wrapper');
-    getLabelDepartureOnDate = () => cy.get('#label-departure-on #trips-selected-date');
     getLabelCalendar = () => cy.get('div #calendar-week');
-    getLabelPassengerDetails = () => cy.get('.passenger-wrapper div.title label');
     getMonthBtn = () => cy.get('.calendar-view-wrapper .calendar-view-month');
     getMonthDropdownList = () => cy.get('select[name="calendar_month"] option');
     getMonthDropdownSelect = () => cy.get('select[name="calendar_month"]');
     getFridayButton = () => cy.get('div .calendar-day-selection-wrapper :nth-child(5)');
+    getWeekButton = () => cy.get('button.calendar-view-week');
+    getSaturdayButton = () => cy.get('div .calendar-day-selection-wrapper :nth-child(6)');
+
+    //Departure on
+    getFirstTripCard = () => cy.get('div .trip:first-child');
+    getTicketsAvailableFirstTripCard = () => cy.get('.trip:first-child span.availability span.num');
+    getLabelDepartureOnDate = () => cy.get('#label-departure-on #trips-selected-date');
+
+    //Passengers details
+    getMainPassengerNameField = () => cy.get('.form-control[name="passenger-name[]"]');
+    getMainPassengerPhoneField = () => cy.get('.iti #passenger-phone');
+    getLabelPassengerDetails = () => cy.get('.passenger-wrapper div.title label');
     getPassengersDetailsDropdown = () => cy.get('.passenger-wrapper .title select.passengers-amount');
     getPassengersDetailsDropdownList = () => cy.get('.layout-wrapper .title select.passengers-amount option');
-    getSeatSelectionDropdown = () => cy.get('.layout-wrapper .title select.passengers-amount');
-    getSeatSelectionDropdownList = () => cy.get('.layout-wrapper .title select.passengers-amount option');
-    getDepartureStationDropdown = () => cy.get('#select2-departure-container');
-    getListDepartureStation = () => cy.get('.select2-results .select2-results__option');
-    getWeekButton = () => cy.get('button.calendar-view-week');
-    getArrivalStationDropdown = () => cy.get('#select2-destination-container');
-    getArrivalStationList = () => cy.get('.select2-results .select2-results__options');
     getPhoneNumberInputFild = () => cy.get('input#passenger-phone');
-    getSelectedSeats = () => cy.get('table.seats tr td[class="seat selected"]');
     getFareTypeDropdown = () => cy.get('[id^=select2-passenger-fare]');
     getFareTypeDropdownList = () => cy.get('label [name="passenger-fare[]"] option');
-    getRowsSeatsSeatSection = () => cy.get('.seat-chart .seats tr:not(tr:first-child)');
     getEmailInputField = () => cy.get(':nth-child(4) > .form-control');
-    getRowsSeatsSeatSection = () => cy.get('.seat-chart .seats tr:not(tr:first-child)');
     getAmountOfChosenPass = () => cy.get('.box-default .passenger-wrapper .passenger-row');
     getLabelMainPassenger  = () => cy.get('div.passenger-row > label');
-    getDepartureStationSelectionDropdown = () => cy.get('select[name="departure"]');
-    getArrivalStationSelectionDropdown = () => cy.get('select[name="destination"]');
-    getSaturdayButton = () => cy.get('div .calendar-day-selection-wrapper :nth-child(6)');
-    getDriverSeat = () => cy.get('table.seats tr td[class="seat blocked"]');  
-    
 
+    //Seat selection
+    getSeatSelectionDropdown = () => cy.get('.layout-wrapper .title select.passengers-amount');
+    getSeatSelectionDropdownList = () => cy.get('.layout-wrapper .title select.passengers-amount option');
+    getSelectedSeats = () => cy.get('table.seats tr td[class="seat selected"]');
+    getRowsSeatsSeatSection = () => cy.get('.seat-chart .seats tr:not(tr:first-child)');
+    //getRowsSeatsSeatSection = () => cy.get('.seat-chart .seats tr:not(tr:first-child)');
+    getDriverSeat = () => cy.get('table.seats tr td[class="seat blocked"]');  
+
+    //Total - Footer section                  
+    
     // Methods
     clickCalendarNextButton() {
         this.getCalendarNextButton().click()
