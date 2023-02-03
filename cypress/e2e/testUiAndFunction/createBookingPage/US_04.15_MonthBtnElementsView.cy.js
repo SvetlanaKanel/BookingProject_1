8a8050d2fa5762fb7f0be82acda5ea25d577fbeb
SvetlanaKanel <47015450+SvetlanaKanel@users.noreply.github.com>
2023-02-02 23:22:39 -0500

    AT_04.15.02_MonthBtnView (#206)
    Co-authored-by: SvetlanaKanel <svetlana.kanel@gmail.come>

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.15_MonthBtnElementsView.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.15_MonthBtnElementsView.cy.js
@@ -12,16 +12,22 @@ describe('US_04.15 | Create booking page > Month button elements view', () => {
         createBookingPage.clickMonthBtn();
     })
 
-    beforeEach(function() {
+    beforeEach(function () {
         cy.fixture('createBookingPage.json').then(createBookingPage => {
             this.createBookingPage = createBookingPage;
         })
     })
 
-    it('AT_04.15.01 Month button is visible, has the attribute "selected" and its background color is #00a65a( rgb(0, 166, 90) )', function() {
+    it('AT_04.15.01 Month button is visible, has the attribute "selected" and its background color is #00a65a( rgb(0, 166, 90) )', function () {
         createBookingPage.getMonthBtn()
             .should('be.visible')
             .and('have.class', 'selected')
             .and('have.css', 'background-color', this.createBookingPage.selectedMonthBtnBackgroundColor);
+    })
+
+    it('AT_04.15.02 | While selected Month button, the Week button does not have the attribute "selected" and its background color is rgb(255, 255, 255)', function () {
+        createBookingPage.getWeekButton()
+            .should('not.have.class', 'selected')
+            .and('have.css', 'background-color', this.createBookingPage.notSelectedWeekBtnBackgroundColor);
     })
 });
