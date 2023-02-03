ec1c9c8a26bd7bb1d100c2912e2a95c5fef179fc
Art Diuzhikov <115650565+adiuzhikov@users.noreply.github.com>
2023-02-02 19:54:28 -0500

    refactor tests (#205)

--- cypress/e2e/testUiAndFunction/leftMenuPanel/us_03.01_MenuPanelMainElements.cy.js
+++ cypress/e2e/testUiAndFunction/leftMenuPanel/us_03.01_MenuPanelMainElements.cy.js
@@ -9,19 +9,23 @@ describe('US_03.01 Menu panel main elements', () => {
     const AGENT = Cypress.env('agent');
 
     beforeEach(function () {
+        cy.fixture('leftMenuPanel').then(leftMenuPanel => {
+            this.leftMenuPanel = leftMenuPanel;
+        });
+
         cy.visit('/');
         cy.login(AGENT.email, AGENT.password);
     });
 
-    it('AT_03.01.01 | Verify the quantity of main elements (icons) in the menu', () => {
-        leftMenuPanel.getMainElementsIcon().should('have.length', 4);
+    it('AT_03.01.01 | Verify the quantity of main elements (icons) in the menu', function() {
+        leftMenuPanel.getMainElementsIcon().should('have.length', this.leftMenuPanel.menuLinks.leftsideMenuPanelLinkNames.length);
     });
 
-    it('AT_03.01.02 | Verify the names of main elements in the menu', () => {
+    it('AT_03.01.02 | Verify the names of main elements in the menu', function() {
         leftMenuPanel.getMainElementsMenuLink().then(($mainElementsNames) => {
             const mainElementsNames = $mainElementsNames.toArray().map(el => el.innerText);
 
-            expect(mainElementsNames).to.deep.equal(['Booking', 'Bookings management', 'Account management', 'Contact us']);
+            expect(mainElementsNames).to.deep.equal(this.leftMenuPanel.menuLinks.leftsideMenuPanelLinkNames);
         });
     });
 });
