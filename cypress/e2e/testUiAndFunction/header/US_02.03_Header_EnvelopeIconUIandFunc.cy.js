7f45127fee5bd3c9eeff57e0a7f871432ec22c4b
Rom-T <93357374+Rom-T@users.noreply.github.com>
2023-02-05 17:29:16 -0500

    RF | US_02.03 (#239)
    * US_02.03

--- cypress/e2e/testUiAndFunction/header/US_02.03_Header_EnvelopeIconUIandFunc.cy.js
+++ cypress/e2e/testUiAndFunction/header/US_02.03_Header_EnvelopeIconUIandFunc.cy.js
@@ -9,22 +9,28 @@ const helpdeskPage = new HelpdeskPage();
 const AGENT = Cypress.env('agent');
 
 describe('US_02.03 Header Envelope icon UI and functionality', () => {
+    before(() => {
+        cy.visit('/');
+        cy.login(AGENT.email, AGENT.password);
+    });
+    
     beforeEach(function () {
         cy.fixture('helpdeskPage').then(helpdeskPage => {
             this.helpdeskPage = helpdeskPage;
-         })
-        cy.visit('/');
-        cy.login(AGENT.email, AGENT.password);
+         });
+    });
+
+    it('AT_02.03.02 | Verify Envelope icon is clickable and redirects to Helpdesk page; filter status is Active', function () {
+        header.clickEnvelopeIcon();
+        helpdeskPage
+            .getHelpdeskHeader()
+            .should('include.text', this.helpdeskPage.headers.mainHeaderPage);
+        helpdeskPage
+            .getHelpdeskFilter()
+            .should('have.text', this.helpdeskPage.filter.selectFilterStatus);
     });
 
     it('AT_02.03.01 | Verify Envelope icon is visible', () => {
         header.getEnvelopeIcon().should('be.visible');
-    })
-
-    it('AT_02.03.02 | Verify Envelope icon is clickable and redirects to Helpdesk page; filter status is Active', function () {
-        header.clickEnvelopeIcon();
-        helpdeskPage.getHelpdeskHeader().should('include.text', this.helpdeskPage.headers.mainHeaderPage);
-        helpdeskPage.getHelpdeskFilter().should('have.text', this.helpdeskPage.filter.selectFilterStatus);
-   });
+    });
 })
-
