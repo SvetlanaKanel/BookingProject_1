2f775da0433605f36b3addca320c9da921d2fd19
Rom-T <93357374+Rom-T@users.noreply.github.com>
2023-02-05 17:29:52 -0500

    US_02.05, US_04.12 (#241)

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.07_ArrivalDropdown.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.07_ArrivalDropdown.cy.js
@@ -3,23 +3,19 @@
 import CreateBookingPage from "../../../pageObjects/CreateBookingPage";
 
 const createBookingPage = new CreateBookingPage();
+const AGENT = Cypress.env('agent');
 
 describe('US_04.07_Arrival Dropdown UI and functionality ', () => {
 
-    const AGENT = Cypress.env('agent');
-
-    beforeEach(function () {
-        cy.fixture('createBookingPage').then(createBookingPage => {
-            this.createBookingPage = createBookingPage;
-        })
-
+    before(() => {
         cy.visit('/');
         cy.login(AGENT.email, AGENT.password);
     });
-
-    it('AT_04.07.01 | Verify the Arrival station Dropdown Menu is clickable and opens up the dropdown menu', function () {
-        createBookingPage.clickArrivalStationDropdown()
-        createBookingPage.getArrivalStationList().should('be.visible')
+    
+    beforeEach(function () {
+        cy.fixture('createBookingPage').then(createBookingPage => {
+            this.createBookingPage = createBookingPage;
+        });
     });
 
     it('AT_04.07.02 | Verify that after clicking on any space outside of the opened drop-down menu, the drop-down menu will close', function () {
@@ -27,5 +23,10 @@ describe('US_04.07_Arrival Dropdown UI and functionality ', () => {
         createBookingPage.clickArrivalStationDropdown();
         createBookingPage.getArrivalSearchField().should('not.exist');
         createBookingPage.getArrivalStationList().should('not.exist');
-     });
+    });
+
+    it('AT_04.07.01 | Verify the Arrival station Dropdown Menu is clickable and opens up the dropdown menu', function () {
+        createBookingPage.clickArrivalStationDropdown()
+        createBookingPage.getArrivalStationList().should('be.visible')
+    });
 })
--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.25_functionality-One passenger.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.25_functionality-One passenger.cy.js
@@ -3,33 +3,40 @@
 import CreateBookingPage from "../../../pageObjects/CreateBookingPage.js";
 
 const createBookingPage = new CreateBookingPage();
+const AGENT = Cypress.env('agent');
 
 describe('US_04.25 | Passengers details functionality - One passenger', () => {
-    const AGENT = Cypress.env('agent');
 
-    beforeEach(function () {
-        cy.fixture('createBookingPage').then(createBookingPage => {
-            this.createBookingPage = createBookingPage;
-        })
+    before(() => {
         cy.visit('/');
         cy.login(AGENT.email, AGENT.password);
 
         //Precondition
-        createBookingPage.clickCalendarNextButton()
-        cy.wait(5000)
-        createBookingPage.clickFirstTripCard()
-        cy.wait(2000)
-    })
+        createBookingPage.clickCalendarNextButton();
+        cy.intercept('/tools/**').as('getTrip');
+		cy.wait('@getTrip');
+        createBookingPage.clickFirstTripCard();
+    });
+
+    beforeEach(function () {
+        cy.fixture('createBookingPage').then(createBookingPage => {
+            this.createBookingPage = createBookingPage;
+        });
+    });
 
     it('AT_04.25.01 | Verify the opportunity to fill main passengers name in "Passenger name" input field', function () {
-        createBookingPage.typeIntoMainPassengerNameField(this.createBookingPage.inputField.main_passenger.name)
+        createBookingPage.typeIntoMainPassengerNameField(this.createBookingPage.inputField.main_passenger.name);
 
-        createBookingPage.getMainPassengerNameField().should('have.value', this.createBookingPage.inputField.main_passenger.name)  
+        createBookingPage
+            .getMainPassengerNameField()
+            .should('have.value', this.createBookingPage.inputField.main_passenger.name);
     });
 
     it('AT_04.25.02 | Verify the opportunity to fill main passengers phone in "Phone number" input field', function () {
-        createBookingPage.typeIntoMainPassengerPhoneField(this.createBookingPage.inputField.main_passenger.phone)
+        createBookingPage.typeIntoMainPassengerPhoneField(this.createBookingPage.inputField.main_passenger.phone);
 
-        createBookingPage.getMainPassengerPhoneField().should('have.value', this.createBookingPage.inputField.main_passenger.phone)
+        createBookingPage
+            .getMainPassengerPhoneField()
+            .should('have.value', this.createBookingPage.inputField.main_passenger.phone);
     });
 })
