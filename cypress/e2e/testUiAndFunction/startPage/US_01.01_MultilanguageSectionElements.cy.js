1f79895edcc5fe06bc6e07c6d9acbf596b7447da
Rom-T <93357374+Rom-T@users.noreply.github.com>
2023-02-05 19:56:47 -0500

    RF | US_03.01 (#245)

--- cypress/e2e/testUiAndFunction/startPage/US_01.01_MultilanguageSectionElements.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/US_01.01_MultilanguageSectionElements.cy.js
@@ -5,20 +5,25 @@ import { StartPage } from "../../../pageObjects/StartPage";
 const startPage = new StartPage();
 
 describe('US_01.01 | Multilanguage section elements UI and functionality', () => {
+    before(() => {
+        cy.visit('/');
+    });
+    
     beforeEach(function () {
         cy.fixture('startPage').then(startPage => {
             this.startPage = startPage;
         });
-        cy.visit('/')
+    });
+
+    it('AT_01.01.01 | Verify Russia flag icon is visible', function () {
+        startPage.getRussiaFlagIcon().should('be.visible');
     });
 
     it('AT_01.01.03 | Thailand flag icon is visible', function () {
-        startPage
-        .getThailandFlagIcon()
-        .should('be.visible');
+        startPage.getThailandFlagIcon().should('be.visible');
     }); 
     
-    it ('AT_01.01.05| British flag icon is visible', ()=>{
+    it ('AT_01.01.05 | British flag icon is visible', () => {
         startPage.getUKFlagIcon().should('be.visible');
     });
 
@@ -27,10 +32,6 @@ describe('US_01.01 | Multilanguage section elements UI and functionality', () =>
         startPage.getLoginButton().should('not.include.text', this.startPage.buttons.loginBtnText);
         startPage.clickUKFlagIcon();
         startPage.getLoginButton().should('include.text', this.startPage.buttons.loginBtnText);
-    });
-
-    it('AT_01.01.01 | Verify Russia flag icon is visible', function () {
-        startPage.getRussiaFlagIcon().should('be.visible');
     });
 
     it('AT_01.01.02 | Verify Russia flag icon: clickable / changing language to Russian', function () {
