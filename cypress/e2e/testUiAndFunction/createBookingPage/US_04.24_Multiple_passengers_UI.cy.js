ddd5fcbe1fa6e59652f4394cdff1bf85e4585dee
Rom-T <93357374+Rom-T@users.noreply.github.com>
2023-02-05 22:46:00 -0500

    RF | US_04.24 (#246)

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.24_Multiple_passengers_UI.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.24_Multiple_passengers_UI.cy.js
@@ -3,42 +3,44 @@
 import CreateBookingPage from "../../../pageObjects/CreateBookingPage"
 
 const createBookingPage = new CreateBookingPage();
-
 const AGENT = Cypress.env('agent');
 
-describe.skip('US_04.24 | Multiple passengers UI', () => {
+describe('US_04.24 | Multiple passengers UI', () => {
 
     beforeEach(function () {
         cy.fixture('createBookingPage').then(createBookingPage => {
             this.createBookingPage = createBookingPage;
-        })
+        });
     });
 
     before(function () {
-        cy.visit('/')
-        cy.login(AGENT.email, AGENT.password)
+        cy.visit('/');
+        cy.login(AGENT.email, AGENT.password);
 
         createBookingPage.clickCalendarNextButton();
-        cy.wait(5000);
         createBookingPage.clickFridayButton();
-        cy.wait(2000);
+        cy.intercept('/tools/**').as('getTrip');
+		cy.wait('@getTrip');
         createBookingPage.clickFirstTripCard();
     });
 
-    it.skip('AT_04.24.01 | Verify that Fare type dropdown menu is visible and has three options: Adult, Child and Elder', function () {
+    it('AT_04.24.01 | Verify that Fare type dropdown menu is visible and has three options: Adult, Child and Elder', function () {
         createBookingPage.clickFareTypeDropdown();
+        createBookingPage
+            .getMainPassengerFareTypeDropdownListOptions()
+            .should('have.length', this.createBookingPage.dropdowns.fareType.faretypesNumber);
+
         createBookingPage.getMainPassengerFareTypeDropdownListOptions().each(($el, i) => {
-            cy.wrap($el).should('have.length', this.createBookingPage.dropdowns.fareType.faretypesNumber);
-        });
-        createBookingPage.getFareTypeDropdownList().each(($el, i) => {
-            cy.wrap($el).should('be.visible')
-            expect($el.text()).to.be.equal(this.createBookingPage.dropdowns.fareType.fareTypesNames[i])
+            cy.wrap($el)
+              .should('be.visible')
+              .and('have.text', this.createBookingPage.dropdowns.fareType.fareTypesNames[i]);
         });
     });
 
     it('AT_04.24.02 | Verify that Fare type dropdown menu by default has title Adult', function () {
-        createBookingPage.getFareTypeDropdown()
+        createBookingPage
+            .getFareTypeDropdown()
             .should('have.attr', 'title')
-            .and('equal', this.createBookingPage.dropdowns.fareType.fareTypesNames[0])
-    })
+            .and('equal', this.createBookingPage.dropdowns.fareType.fareTypesNames[0]);
+    });
 });    
