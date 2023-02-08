18b6c47b1eaca640ef113bcca514f8bf87a481cd
Olga <oforostinko@gmail.com>
2023-02-08 03:14:19 +0300

    test AT_03.04.03

--- cypress/e2e/testUiAndFunction/leftMenuPanel/US_03.04_AccountManagementLink.cy.js
+++ cypress/e2e/testUiAndFunction/leftMenuPanel/US_03.04_AccountManagementLink.cy.js
@@ -1,9 +1,11 @@
 /// <reference types="Cypress"/>
 
 import LeftMenuPanel from "../../../pageObjects/LeftMenuPanel";
+import AccountManagementPage from "../../../pageObjects/AccountManagementPage";
 
 const leftMenuPanel = new LeftMenuPanel();
 const AGENT = Cypress.env('agent');
+const accountManagementPage = new AccountManagementPage();
 
 describe('US_03.04 Account management link', () => {
 
@@ -16,6 +18,9 @@ describe('US_03.04 Account management link', () => {
         cy.fixture('leftMenuPanel').then(leftMenuPanel => {
             this.leftMenuPanel = leftMenuPanel;
         });
+        cy.fixture('accountManagementPage').then(accountManagementPage => {
+            this.accountManagementPage = accountManagementPage;
+        });
     });
 
     it('AT_03.04.01 Verify that "Account management" icon in the left menu panel is visible', function () {
@@ -26,5 +31,13 @@ describe('US_03.04 Account management link', () => {
         leftMenuPanel
             .getAccountManagementMenuLink()
             .should('include.text', this.leftMenuPanel.menuLinks.accountLink);
+    });
+
+    it('AT_03.04.03 Clicking "Account management" opening the page with heading "Account management"', function () {
+        leftMenuPanel.clickAccountManagementIcon() 
+
+        accountManagementPage 
+            .getAccountManagementHeader()
+            .should('include.text', this.accountManagementPage.headers.accountManagementHeader)       
     });
 })
