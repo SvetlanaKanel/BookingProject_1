52ed044118d9e1089bbedd20dc2b04e8d46f7263
MARINA MONROE <82535878+MarinaMone@users.noreply.github.com>
2023-02-11 15:54:49 -0500

    Mm/04.31.02.04.31.03 (#312)
    * skip_04/12
    
    * skip_04.12
    
    * add_04.31

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.31_TotalUI.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.31_TotalUI.cy.js
@@ -14,4 +14,12 @@ describe('US_04.31 | Total UI', () => {
     it('AT_04.31.01 | Verify that the text "Total " is visible', () => {
         createBookingPage.getTotalPriceLabel().should('be.visible');    
     });
+
+    it('AT_04.31.02 | Verify that the Total has no value by default', () => {   
+        createBookingPage.getTotalPrice().should('have.text', '');
+    });    
+    
+     it('AT_04.31.03 |  Verify that button “Reset” is visible and has a font color #00a65a)', () => {
+        createBookingPage.getResetButton().should('be.visible')
+     });    
 })
