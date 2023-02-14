88586b475a6cdf08ea54cc2c765e992cd9fa34b1
safbek <safbek@list.ru>
2023-02-14 13:13:23 +0300

    move warning text to fixtures folder

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.21_NotAvailableTripCardUI.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.21_NotAvailableTripCardUI.cy.js
@@ -7,13 +7,19 @@ const createBookingPage = new CreateBookingPage();
 
 describe('US_04.21 | Not available trip card UI', () => {
 
-    before(() => {
+    before(function () {
         cy.visit('/');
         cy.login(AGENT.email, AGENT.password);
         cy.viewport(1280, 720)
     })
 
-    it('AT_04.21.01 | Verify the warning text displays', () => {
+    beforeEach(function () {
+        cy.fixture('createBookingPage').then(createBookingPage => {
+            this.createBookingPage = createBookingPage;
+        })
+    })
+
+    it('AT_04.21.01 | Verify the warning text displays', function () {
 
         createBookingPage
             .selectDepartureStation('Chonburi')
@@ -27,7 +33,7 @@ describe('US_04.21 | Not available trip card UI', () => {
         cy
             .get('.nothing')
             .should('be.visible')
-            .and('have.text', 'Trips are not available. Try searching for another date or location.')
+            .and('have.text', this.createBookingPage.warningTexts.notAvailableTripText)
     })
 
 });
