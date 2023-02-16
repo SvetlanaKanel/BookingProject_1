27c7cb3d026b272914a4bcb32a0ba95738ec648a
safbek <safbek@list.ru>
2023-02-16 17:11:14 +0300

    add AT_04.21.02 | Verify background and border color

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.21_NotAvailableTripCardUI.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.21_NotAvailableTripCardUI.cy.js
@@ -2,38 +2,43 @@
 
 import CreateBookingPage from "../../../pageObjects/CreateBookingPage";
 
-const AGENT = Cypress.env('agent');
 const createBookingPage = new CreateBookingPage();
 
 describe('US_04.21 | Not available trip card UI', () => {
+    const AGENT = Cypress.env('agent');
 
     before(function () {
         cy.visit('/');
         cy.login(AGENT.email, AGENT.password);
-        cy.viewport(1280, 720)
+
+        createBookingPage.selectDepartureStation('Chonburi')
+        createBookingPage.selectArrivalStation('Bangkok Khao San')
+        createBookingPage.clickSaturdayButton()
     })
 
     beforeEach(function () {
         cy.fixture('createBookingPage').then(createBookingPage => {
             this.createBookingPage = createBookingPage;
         })
+
+        cy.fixture('colors').then(colors => {
+            this.colors = colors;
+        })
     })
 
     it('AT_04.21.01 | Verify the warning text displays', function () {
-
-        createBookingPage
-            .selectDepartureStation('Chonburi')
-
-        createBookingPage
-            .selectArrivalStation('Bangkok Khao San')
-
-        createBookingPage
-            .clickSaturdayButton()
-
         cy
             .get('.nothing')
             .should('be.visible')
             .and('have.text', this.createBookingPage.warningTexts.notAvailableTripText)
+    })
+
+    it('AT_04.21.02 | Verify that block has background-color: #F6EAEA and solid border-color: #B84D4D', function () {
+        cy
+            .get('.nothing')
+            .should('have.css', 'background-color', this.colors.redWarningBg)
+            .and('have.css', 'border-color', this.colors.redWarningBorder)
+            .and('have.css', 'border-style', 'solid')
     })
 
 });
