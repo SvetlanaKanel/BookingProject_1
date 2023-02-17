50292a1428bc3f2b39d8e4674edfa963f57852fd
Rom-T <93357374+Rom-T@users.noreply.github.com>
2023-02-17 18:34:03 -0500

    RF US_05.01 (#361)

--- cypress/e2e/testUiAndFunction/bookingsListPage/US_05.01_SearchSectionUI.cy.js
+++ cypress/e2e/testUiAndFunction/bookingsListPage/US_05.01_SearchSectionUI.cy.js
@@ -34,35 +34,7 @@ describe('US_05.01 Booking list page >Top Section> Search Form UI', () => {
         bookingsListPage.getSearchField().should('have.attr', 'placeholder', this.bookingsListPage.inputFields.searchPlaceholder);
 
     });
-      
-    it('AT_05.01.01 Verify that Booking ID text field with a placeholder "Booking ID" displayed', function () {
-        
-        bookingsListPage.getBookingIdField().should('be.visible');
-        bookingsListPage.getBookingIdField().should('have.attr', 'placeholder', this.bookingsListPage.inputFields.bookingIdPlaceholder);
 
-    }); 
-
-    it('AT_05.01.01 Verify that Status text field with a placeholder "Status" displayed', function () {
-        
-        bookingsListPage.getStatusField().should('be.visible');
-        bookingsListPage.getStatusField().should('have.attr', 'placeholder', this.bookingsListPage.inputFields.statusPlaceholder);
-
-    }); 
-
-    it('AT_05.01.01 Verify that Route field with a  default text "Route" displayed', function () {
-        
-        bookingsListPage.getRouteField().should('be.visible');
-        bookingsListPage.getRouteField().should('have.text', this.bookingsListPage.inputFields.routeText);
-
-    }); 
-
-    it('AT_05.01.01 Verify that Vehicle field with a default text "Vehicle" displayed', function () {
-
-        bookingsListPage.getVehicleField().should('be.visible');
-        bookingsListPage.getVehicleField().should('have.text', this.bookingsListPage.inputFields.vehicleText);
-
-    }); 
-   
     it('AT_05.01.02 Verify that the link “Clear” displayed', function () {
         
         bookingsListPage
@@ -84,5 +56,4 @@ describe('US_05.01 Booking list page >Top Section> Search Form UI', () => {
                 })
         }
     })
-
 })
