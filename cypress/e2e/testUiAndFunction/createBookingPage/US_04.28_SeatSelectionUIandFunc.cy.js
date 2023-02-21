e994bb3d5ddd00e2b68f69eb139f39bacdcc249f
SviatlanaMag <103537254+SvetlanaMag@users.noreply.github.com>
2023-02-21 20:31:17 +0300

    RF | AT_04.28.12 > replacing the random selection of the number of passengers with the selection of the boundary values (#376)
    * RF AT_04.28.12
    
    * move comma to line 127

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.28_SeatSelectionUIandFunc.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.28_SeatSelectionUIandFunc.cy.js
@@ -31,6 +31,10 @@ describe('US_04.28 | Seat selection UI and functionality', () => {
             createBookingPage.clickFirstTripCard()
         });
 
+        it('AT_04.28.01|The section name "Seat selection" is visible', function (){
+            createBookingPage.getLabelSeatSelection().should('have.text', this.createBookingPage.seatSelectoinLabel)
+        });
+
         it('AT_04.28.02 | "Seat selection dropdown" is visible and displays the amount of passengers, selected in the "Passengers details dropdown"', () => {
             createBookingPage.getRandomPassengersAmmount().then(($el) => {
                 const passengersAmount = $el
@@ -102,9 +106,7 @@ describe('US_04.28 | Seat selection UI and functionality', () => {
                 cy.login(AGENT.email, AGENT.password)
         
                 createBookingPage.selectDepartureStation('Bangkok Khao San')
-                        
                 createBookingPage.selectArrivalStation('Chonburi')
-            
                 createBookingPage.clickCalendarNextButton()
                 createBookingPage.clickSaturdayButton()
                 waitForToolsPing()
@@ -141,140 +143,150 @@ describe('US_04.28 | Seat selection UI and functionality', () => {
 
     describe('US_04.28 | Seat selection functionality', () => {
 
-        beforeEach(() => {
-            cy.visit('/')
-            cy.login(AGENT.email, AGENT.password)
-            
-            createBookingPage.clickCalendarNextButton()
-            waitForToolsPing()
-            createBookingPage.clickCalendarNextButton()
-            waitForToolsPing()
-            createBookingPage.clickFirstTripCard()
-        });
+        context('Testcases without booking/reservation', () => {
+            beforeEach(function () {
+                cy.visit('/')
+                cy.login(AGENT.email, AGENT.password)
+        
+                createBookingPage.selectDepartureStation(this.createBookingPage.dropdowns.departureStation.stationsNames[7])
+                createBookingPage.clickCalendarNextButton()
+                waitForToolsPing()
+                createBookingPage.clickFirstTripCard()
+            });
 
-        it('AT_04.28.03 | Verify number of default selected seats equals number of selected passengers from passenger details dropdown menu', () => {
-            createBookingPage.getPassengersDetailsDropdownList().then($el => {
-                let numberOfPass = getRandomElementOfArray($el)  
-                createBookingPage.getPassengersDetailsDropdown()
-                    .select(numberOfPass)
-                    .invoke('val').then((value) => {
-                        let chosenNumOfPassengers = +value
-    
-                        createBookingPage.getSelectedSeats()
-                            .then(($el) => {
-                            let defaultNumberOfSelectedSeats = $el.length
-                                expect(defaultNumberOfSelectedSeats).to.eq(chosenNumOfPassengers)
+            it('AT_04.28.03 | Verify number of default selected seats equals number of selected passengers from passenger details dropdown menu', () => {
+                createBookingPage.getPassengersDetailsDropdownList().then($el => {
+                    let numberOfPass = getRandomElementOfArray($el)  
+                    createBookingPage.getPassengersDetailsDropdown()
+                        .select(numberOfPass)
+                        .invoke('val').then((value) => {
+                            let chosenNumOfPassengers = +value
+        
+                            createBookingPage.getSelectedSeats()
+                                .then(($el) => {
+                                let defaultNumberOfSelectedSeats = $el.length
+                                    expect(defaultNumberOfSelectedSeats).to.eq(chosenNumOfPassengers)
+                            })
                         })
-                    })
-            })
-        });
+                })
+            });
 
-        it('AT_04.28.07 | The number of available seats in the "Seat selection" section is equal the number of available seats in the selected trip', function() {      
-            createBookingPage.typeIntoMainPassengerNameField(this.createBookingPage.inputField.main_passenger.name)         
-            createBookingPage.clickReservationTicketArrow();
-            createBookingPage.clickReservationTicketButton();   
-            cy.intercept('/tools/ping/**').as('getPopUp')
-            cy.wait('@getPopUp') 
-            bookingPopup.clickCloseBtnBookingPopup()             
-            createBookingPage.clickFirstTripCard()
-    
-            let availableSeatsSeatSelection
-            createBookingPage.getAvailableSeatsSeatSelection().then($el => {
-                availableSeatsSeatSelection = $el.toArray().length                 
-            })       
-    
-            createBookingPage.getTicketsAvailableFirstTripCard().then($el => {
-                let availableSeatsSelectedTrip = $el.text()
-                
-                expect(availableSeatsSeatSelection).to.eq(+availableSeatsSelectedTrip)        
-            })    
-        });
-
-        it('AT_04.28.09 | When unselecting the seat in the "Seats table" in the "Summary" section the red color text "Select seat" appears', function () {
-            createBookingPage.getSeatSelectionDropdownList().then($el => {
-                let amountOfPass = getRandomElementOfArray($el);
-        
-                createBookingPage.getSeatSelectionDropdown()
-                    .select(amountOfPass)
-        
-                createBookingPage.getSelectedSeats().then(($el) => {
-                    const selectedSeatsArr = getArray($el)
-                        
-                    let indexOfSeat = Math.floor(Math.random() * selectedSeatsArr.length)
-                    let seatNumber = selectedSeatsArr[indexOfSeat]
-        
-                    createBookingPage.getSelectedSeats()
-                        .contains(seatNumber)
-                        .click()
-                        
-                    createBookingPage.getSeatsNumberColumnSummary().then(($el) => {
-                        let seatsSummaryArrayAfter = getArray($el)
-    
-                        expect(seatsSummaryArrayAfter[indexOfSeat]).to.eq(this.createBookingPage.summarySection.seatsColumn.warningText)
-                        cy.wrap($el)
-                            .contains(this.createBookingPage.summarySection.seatsColumn.warningText)
-                            .should('have.class', 'text-red')
-    
-                        createBookingPage.getAllSeatsSeatSelection()
+            it('AT_04.28.09 | When unselecting the seat in the "Seats table" in the "Summary" section the red color text "Select seat" appears', function () {
+                createBookingPage.getSeatSelectionDropdownList().then($el => {
+                    let amountOfPass = getRandomElementOfArray($el);
+            
+                    createBookingPage.getSeatSelectionDropdown()
+                        .select(amountOfPass)
+            
+                    createBookingPage.getSelectedSeats().then(($el) => {
+                        const selectedSeatsArr = getArray($el)
+                            
+                        let indexOfSeat = Math.floor(Math.random() * selectedSeatsArr.length)
+                        let seatNumber = selectedSeatsArr[indexOfSeat]
+            
+                        createBookingPage.getSelectedSeats()
                             .contains(seatNumber)
                             .click()
-                            .should('have.class', 'selected')
-                    })
-                })
-            })
-        });
-
-        it('AT_04.28.11 | Verify custom seat selection by window and next two ones in each row for chosen number of passengers watches assigned seats in passenger details section', () => {
-            let index = getIntergerMinInclMaxExcl(2, 11)
-            createBookingPage.getPassengersDetailsDropdown()
-                .select(index)
-                .invoke('val').then((value) => {
-                    let chosenNumOfPassengers = +value
-
-                    createBookingPage.getSelectedSeats().click({ multiple: true })
-                       
-                    for (let i = 1; i <= Math.ceil(chosenNumOfPassengers / 3); i++) {
-                        createBookingPage.getAllSeatsSeatSelection()
-                            .filter('.available')
-                            .contains('A')
-                            .first().click()
-                            .next().click()
-                            .next().click()
-                    }
-
-                    createBookingPage.getSelectedSeats().then(($el) => {
-                        let arrayOfCustomSeletedSeats = $el.text()
-
-                        createBookingPage.getPassengerDetailsAssignedSeats().then(($el) => {
-                            let arrayOfAssignedSeats = $el.text()
-                            expect(arrayOfAssignedSeats).to.deep.eq(arrayOfCustomSeletedSeats)
+                            
+                        createBookingPage.getSeatsNumberColumnSummary().then(($el) => {
+                            let seatsSummaryArrayAfter = getArray($el)
+        
+                            expect(seatsSummaryArrayAfter[indexOfSeat]).to.eq(this.createBookingPage.summarySection.seatsColumn.warningText)
+                            cy.wrap($el)
+                                .contains(this.createBookingPage.summarySection.seatsColumn.warningText)
+                                .should('have.class', 'text-red')
+        
+                            createBookingPage.getAllSeatsSeatSelection()
+                                .contains(seatNumber)
+                                .click()
+                                .should('have.class', 'selected')
                         })
                     })
                 })
-        });
-    
-        it.skip('AT_04.28.12 | Verify, that default numbers of the selected seats in the "Seats table" and the numbers of the seats in the "Passenger details" section are equal', function () {
-            createBookingPage.getRandomPassengersAmmount().then($el => {
-                let passengersAmount = $el;
-    
-                createBookingPage.getSeatSelectionDropdown()
-                    .select(passengersAmount)
-    
-                createBookingPage.getSelectedSeats().then(($el) => {
-                    const selectedSeatsArray = getArray($el)
-         
-                    createBookingPage.getPassengerDetailsAssignedSeats().then(($el) => {
-                    const seatsNumberArray = getArray($el)
-         
-                    expect(seatsNumberArray).to.deep.eq(selectedSeatsArray)
+            });
+
+            it('AT_04.28.11 | Verify custom seat selection by window and next two ones in each row for chosen number of passengers watches assigned seats in passenger details section', () => {
+                let index = getIntergerMinInclMaxExcl(2, 11)
+                createBookingPage.getPassengersDetailsDropdown()
+                    .select(index)
+                    .invoke('val').then((value) => {
+                        let chosenNumOfPassengers = +value
+
+                        createBookingPage.getSelectedSeats().click({ multiple: true })
+                        
+                        for (let i = 1; i <= Math.ceil(chosenNumOfPassengers / 3); i++) {
+                            createBookingPage.getAllSeatsSeatSelection()
+                                .filter('.available')
+                                .contains('A')
+                                .first().click()
+                                .next().click()
+                                .next().click()
+                        }
+
+                        createBookingPage.getSelectedSeats().then(($el) => {
+                            let arrayOfCustomSeletedSeats = $el.text()
+
+                            createBookingPage.getPassengerDetailsAssignedSeats().then(($el) => {
+                                let arrayOfAssignedSeats = $el.text()
+                                expect(arrayOfAssignedSeats).to.deep.eq(arrayOfCustomSeletedSeats)
+                            })
+                        })
                     })
-                })
-            })
-        });
+            });
         
-        it('AT_04.28.01|The section name "Seat selection" is visible', function (){
-            createBookingPage.getLabelSeatSelection().should('have.text', this.createBookingPage.seatSelectoinLabel)
+            it('AT_04.28.12 | Verify, that default numbers of the selected seats in the "Seats table" and the numbers of the seats in the "Passenger details" section are equal (for 1, 60, 300 passengers)', function () {
+                let passengersAmountBoundaryArray = this.createBookingPage.passengersAmountBoundaryArray300
+                for(let passengersAmount of passengersAmountBoundaryArray){
+                    createBookingPage.getSeatSelectionDropdown()
+                        .select(passengersAmount)
+                    createBookingPage.getSelectedSeats().then(($el) => {
+                        let selectedSeatsNumbers = getArray($el)
+                        expect(selectedSeatsNumbers.length).to.eq(parseInt(passengersAmount))
+        
+                        createBookingPage.getPassengerDetailsAssignedSeats().then(($el) => {
+                            const seatsNumberArray = getArray($el)
+                    
+                            expect(seatsNumberArray).to.deep.eq(selectedSeatsNumbers)
+                        })
+                    })
+                }
+            });
         });
+
+        context('Testcases with booking/reservation', () => {
+           
+            beforeEach(() => {
+                cy.visit('/')
+                cy.login(AGENT.email, AGENT.password)
+                
+                createBookingPage.clickCalendarNextButton()
+                waitForToolsPing()
+                createBookingPage.clickCalendarNextButton()
+                waitForToolsPing()
+                createBookingPage.clickFirstTripCard()
+            });
+
+            it('AT_04.28.07 | The number of available seats in the "Seat selection" section is equal the number of available seats in the selected trip', function() {      
+                createBookingPage.typeIntoMainPassengerNameField(this.createBookingPage.inputField.main_passenger.name)         
+                createBookingPage.clickReservationTicketArrow();
+                createBookingPage.clickReservationTicketButton();   
+                cy.intercept('/tools/ping/**').as('getPopUp')
+                cy.wait('@getPopUp') 
+                bookingPopup.clickCloseBtnBookingPopup()             
+                createBookingPage.clickFirstTripCard()
+        
+                let availableSeatsSeatSelection
+                createBookingPage.getAvailableSeatsSeatSelection().then($el => {
+                    availableSeatsSeatSelection = $el.toArray().length                 
+                })       
+        
+                createBookingPage.getTicketsAvailableFirstTripCard().then($el => {
+                    let availableSeatsSelectedTrip = $el.text()
+                    
+                    expect(availableSeatsSeatSelection).to.eq(+availableSeatsSelectedTrip)        
+                })    
+            });
+        });    
     });
 });
 
