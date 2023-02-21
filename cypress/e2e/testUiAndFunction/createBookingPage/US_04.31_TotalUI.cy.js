ece3a158814e1bb1fc51d4493e467b1ce8d756fb
MARINA MONROE <82535878+MarinaMone@users.noreply.github.com>
2023-02-21 01:23:12 -0500

    AT_04.31.04 | Verify that button “Reset” has a font color (#00a65a) (#382)
    * skip_04/12
    
    * skip_04.12
    
    * change_name
    
    * AT_04.31.04

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.31_TotalUI.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.31_TotalUI.cy.js
@@ -11,6 +11,12 @@ describe('US_04.31 | Total UI', () => {
 		cy.login(AGENT.email, AGENT.password);
     });
 
+    beforeEach(function () {
+        cy.fixture('colors').then(colors => {
+            this.colors = colors;
+        });
+	});
+
     it('AT_04.31.01 | Verify that the text "Total " is visible', () => {
         createBookingPage.getTotalPriceLabel().should('be.visible');    
     });
@@ -19,7 +25,11 @@ describe('US_04.31 | Total UI', () => {
         createBookingPage.getTotalPrice().should('have.text', '');
     });    
     
-     it('AT_04.31.03 |  Verify that button “Reset” is visible and has a font color #00a65a)', () => {
+    it('AT_04.31.03 |  Verify that button “Reset” is visible )', () => {
         createBookingPage.getResetButton().should('be.visible')
-     });    
+    });
+     
+    it('AT_04.31.04 | Verify that button “Reset” has a font color (#00a65a)', function () {
+        createBookingPage.getResetButton().should('have.css', 'color', this.colors.greenBookingPage)
+    });
 })
