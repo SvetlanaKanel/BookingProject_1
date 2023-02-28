bf874e6195e1028d54fe5b8a1cd9394711d49495
YurySelivanov <105432578+YurySelivanov@users.noreply.github.com>
2023-02-28 17:14:21 +0300

    add AT_04.19.02 (#450)

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.19_UnselectedTripCardAvailable.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.19_UnselectedTripCardAvailable.cy.js
@@ -16,10 +16,22 @@ describe('US_04.19 | Unselected trip card available UI', function() {
         waitForToolsPing();          
     });
 
+    beforeEach(function () {
+        cy.fixture('colors').then(colors => {
+            this.colors = colors;
+        }) 
+    });
+
     it('AT_04.19.01 | Verify Trips card with "Number available tickets" label is visible as unselected', function() {
         createBookingPage.getDepartureTripCardsList().each($el => {
             cy.wrap($el).should('be.visible')
                         .and('not.have.class', 'selected')  
+        })
+    });
+
+    it('AT_04.19.02 | Verify Background color of Trip cards is white (#FFF)', function() {
+        createBookingPage.getDepartureTripCardsList().each($el => {
+            cy.wrap($el).should('have.css', 'lighting-color', this.colors.white)  
         })
     });
 });
