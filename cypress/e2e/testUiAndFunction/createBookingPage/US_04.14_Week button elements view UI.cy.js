237cf9e9c9594c0da925152b428ba971c84a55b4
Aly <65147059+Albina-star@users.noreply.github.com>
2023-03-01 09:16:57 -0500

    AT_04.14.02 The Week button is located in the upper right corner of the section Departure date, visible and have text "Week" (#445)
    * AT_04.14.02| The Week button
    
    * made correction
    
    * conflict resolve
    
    * conflikt resolved

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.14_Week button elements view UI.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.14_Week button elements view UI.cy.js
@@ -15,7 +15,7 @@ describe('US_04.14 | Week button elements view ', () => {
 
     beforeEach(function() {
         cy.fixture('createBookingPage').then(createBookingPage => {
-            this.createBookingPage = createBookingPage;
+           this.createBookingPage = createBookingPage;
         });
 
         cy.fixture('colors').then(colors => {
@@ -25,5 +25,12 @@ describe('US_04.14 | Week button elements view ', () => {
 
     it('AT_04.14.01 | In the upper left corner of the section there is a label Departure date which has the color #666666', function() {
         createBookingPage.getLableDepartureDate().should('have.css','color', this.colors.greyHeader)
+    });
+
+    it('AT_04.14.02| The Week button is located in the upper right corner of the section Departure date, visible and have text "Week"', function () {
+        createBookingPage.getWeekButton()
+        .should('be.visible')
+        .and('have.text', this.createBookingPage.weekButtonText)
+        .and('have.css','position', 'static')
     });
 });    
