628420527f3f80cd24fdcd8b4d0cb7af6408f9ab
Olga Forostinko <oforostinko@gmail.com>
2023-03-10 08:39:21 -0800

    changed the logic

--- cypress/e2e/testUiAndFunction/startPage/us_01.06_LoginByEmailTub.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/us_01.06_LoginByEmailTub.cy.js
@@ -9,28 +9,39 @@ const loginPopup = new LoginPopup();
 const startPage = new StartPage();
 
 describe('US_01.06 | Login by email tub functionality', { tags: ['smoke'] }, () => {
+    const AGENT = Cypress.env('agent')
 
-    const AGENT = Cypress.env('agent');
-
-    beforeEach(function () {
+    before(function () {
         cy.then(Cypress.session.clearCurrentSessionData);
         cy.visit('/');
-        cy.fixture('createBookingPage').then(bookingData => {
-            this.bookingData = bookingData
-        });
-        cy.fixture('colors').then(colors => {
-            this.colors = colors;
-        });
+        startPage.clickLoginButton();
     });
 
-    it('AT_01.06.01 | Verify Sign In Button redirect to the Create Booking Page', { tags: ['regression'] }, function () {
-        cy.login(AGENT.email, AGENT.password);
-        createBookingPage.getCreateBookingHeader().should('include.text', this.bookingData.headers.mainHeaderPage)
+    beforeEach(function () {
+        cy.fixture('startPage').then(startPage => {
+            this.startPage = startPage;
+        });
+
+        cy.fixture('createBookingPage').then(bookingData => {
+            this.bookingData = bookingData;
+        });
+
+        cy.fixture('colors').then(colors => {
+            this.colors = colors;
+        });  
     });
 
     it('AT_01.06.02 | Color of Sign In Button', function () {
-        startPage.clickLoginButton();
         loginPopup.getByEmailSignInButton().should('have.css','color', this.colors.greenPopup)
+    });
+
+    it('AT_01.06.01 | Verify Sign In Button redirect to the Create Booking Page', { tags: ['regression'] }, function () {
+        loginPopup.enterEmail(AGENT.email)
+        loginPopup.enterPassword(AGENT.password)
+        loginPopup.clickByEmailSignInButton()
+        createBookingPage
+            .getCreateBookingHeader()
+            .should('include.text', this.bookingData.headers.mainHeaderPage)
     });
 });
 
