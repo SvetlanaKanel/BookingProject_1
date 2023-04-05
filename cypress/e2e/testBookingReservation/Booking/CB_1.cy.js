46060180081052597154a88f5072b48204c87109
Rom-T <93357374+Rom-T@users.noreply.github.com>
2023-04-04 21:24:10 -0400

    RF CB_1 (#551)

--- cypress/e2e/testBookingReservation/Booking/CB_1.cy.js
+++ cypress/e2e/testBookingReservation/Booking/CB_1.cy.js
@@ -6,39 +6,32 @@ import BookingPopup from "../../../pageObjects/BookingPopup";
 
 const createBookingPage = new CreateBookingPage();
 const bookingPopup = new BookingPopup();
+
+const BOOKING = require('../../../fixtures/createBookingPage.json');
 const AGENT = Cypress.env('agent');
 
 
-beforeEach(function () {
-    cy.cleanData();
-    cy.loginWithSession(AGENT.email, AGENT.password);
-    cy.visit('/');
-
-    cy.fixture('createBookingPage').then(bookingData => {
-        this.bookingData = bookingData;
-    });
-
-    cy.fixture('bookingPopup').then(bookingPopup => {
-        this.bookingPopup = bookingPopup;
-    });
-})
-
 describe('Popup window parameters verification after the booking was completed', { tags: ['regression'] }, function () {
 
-    it('AT_CB_1.01 | Verify Booking status is Pending', function () {
-        const filteredPassengers = this.bookingData.passengers.filter((passenger) => passenger.fareType === 'elder' );
-        const passengerNames = filteredPassengers.map((passenger) => passenger.name);
-        const elderFareTypes = filteredPassengers.map((passenger) => passenger.fareType);
-        const passengerAmount = 2;
+    before(() => {
+        cy.cleanData();
+        cy.loginWithSession(AGENT.email, AGENT.password);
+        cy.visit('/');
     
-        createBookingPage.createBooking(passengerNames, passengerAmount, elderFareTypes)
-
+        createBookingPage.createCustomBooking(BOOKING.defaultBooking);
+    });
+    
+    beforeEach(function () {
+        cy.fixture('bookingPopup').then(bookingPopup => {
+            this.bookingPopup = bookingPopup;
+        });
+    });
+    
+    it('CB_1.01 | Verify Booking status is Pending', function () {
         bookingPopup.getBookingStatus().should('contain', this.bookingPopup.bookingStatus);
     });
 
     it('CB_1.06 | Verify Ticket price', function () {
-        createBookingPage.createCustomBooking(this.bookingData.defaultBooking)
-
         bookingPopup.getTicketsPrice().should('have.text', this.bookingPopup.defaultBookingDetails.price)
         bookingPopup.getFirstFareTypePrice().should('have.text', this.bookingPopup.defaultBookingDetails.price)
         bookingPopup.getTotalPrice().should('have.text', this.bookingPopup.defaultBookingDetails.price)
