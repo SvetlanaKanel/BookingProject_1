40ddfff5beeeb875aa78c12bc109b072a8a9b9ff
OLgaByR <114132439+OLgaByR@users.noreply.github.com>
2023-05-14 15:20:26 +0200

    CB_2.03 | Verify Credit balance on account management page decreased correctly after booking (#614)
    * first commit
    
    * add inpercept to account Manag page
    
    * add assert to intercepts
    
    * change assert about response body
    
    * add cy.wait('@getTrip')
    
    * delete only
    
    * add visible assert
    
    * add cy.wait
    
    * add then and assert about visibility
    
    * return cy.wait(400)
    
    * change assert

--- cypress/e2e/testBookingReservation/Booking/CB_2.cy.js
+++ cypress/e2e/testBookingReservation/Booking/CB_2.cy.js
@@ -14,52 +14,67 @@ const accountManagementPage = new AccountManagementPage()
 const BOOKING = require('../../../fixtures/createBookingPage.json')
 const AGENT = Cypress.env('agent')
 
+const extractDigits = (str) => str.replace(/[^\d.+]/g, '')
+
 describe('Change of balance after booking', { tags: ['regression'] }, function () {
 
     before(() => {
         cy.cleanData()
         cy.loginWithSession(AGENT.email, AGENT.password)
         cy.visit('/');
-
         cy.intercept('POST', '/booking/', (req) => {
             if (req.body.includes('action=get-trips')) {
-				req.alias = 'getTrip'
-			}
-			if (req.body.includes('action=book-ticket')) {
-				req.alias = 'waitForBookedTicket'
-			}
+                req.alias = 'getTrip'
+            }
+            if (req.body.includes('action=book-ticket')) {
+                req.alias = 'waitForBookedTicket'
+            }
             if (req.body.includes('action=get-balance')) {
-				req.alias = 'getBalance'
-			}
+                req.alias = 'getBalance'
+            }
         });
-
         cy.intercept('POST', '/orders').as('getPopUp');
+
+        leftMenuPanel.clickAccountManagementIcon()
+        accountManagementPage.getBalanceAmountOnAccountManagementPage().should('include.text', '$')
+        getAmountFormat(accountManagementPage.getBalanceAmountOnAccountManagementPage()).as('balanceBeforeBooking')
+        leftMenuPanel.clickBookingIcon()
 
         createBookingPage.getBalanceAmountOnBookingPage().should('include.text', '$');
         getAmountFormat(createBookingPage.getBalanceAmountOnBookingPage()).as('ACB');
 
         createBookingPage.createCustomBooking(BOOKING.defaultBooking)
-        
-        cy.wait('@waitForBookedTicket');
-        cy.wait('@getBalance');
-        cy.wait('@getPopUp'); 
+        cy.wait('@waitForBookedTicket')
+        cy.wait('@getBalance')
+        cy.wait('@getPopUp')
+
         getAmountFormat(bookingPopup.getTotalSumm()).as('TTS');
-        
         bookingPopup.getBookingDateWithTime().as('expectedBookingDate')
         bookingPopup.getBookingIDNumber().as('expectedBookingId')
         bookingPopup.getBookingNegativeFullTicketPrice().as('expectedNegativeBookingAmount')
+        bookingPopup.clickCloseBtnBookingPopup()
+        cy.wait(400)
     });
 
     it('CB_2.01 | Verify Credit balance after booking decrease correctly', function () {
-        getAmountFormat(createBookingPage.getBalanceAmountOnBookingPage()).should('eq', Number(this.ACB - this.TTS));
+        getAmountFormat(createBookingPage.getBalanceAmountOnBookingPage()).should('eq', Number(this.ACB - this.TTS)) 
     });
 
     it('CB_2.02 | Verify the Booking record (the date, the bookingId and the amount) BookingPopup page and Account Management page is equal for 1 passenger', function () {
-        bookingPopup.clickCloseBtnBookingPopup()
+        cy.intercept('POST', '/booking/', (req) => {
+            if (req.body.includes('action=get-balance')) {
+                req.alias = 'getBalance'
+            }
+        })
         leftMenuPanel.clickAccountManagementIcon()
+        cy.wait('@getBalance')
 
         accountManagementPage.getBookingDateTime().should('have.text', this.expectedBookingDate)
         accountManagementPage.getBookingDescription().should('contain.text', this.expectedBookingId)
-        getAmountFormat(accountManagementPage.getBookingAmount()).should('equal',this.expectedNegativeBookingAmount)
+        getAmountFormat(accountManagementPage.getBookingAmount()).should('equal', this.expectedNegativeBookingAmount)
     })
-})
+
+    it('CB_2.03 | Verify Credit balance on account management page decreased correctly after booking', function () {
+        getAmountFormat(accountManagementPage.getBalanceAmountOnAccountManagementPage()).should('eq', this.balanceBeforeBooking - this.TTS)
+    });
+});
